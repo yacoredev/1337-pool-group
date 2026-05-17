d2b5da4eeb37fca0eff32a7d41976c2b15f76520
Yacin-lab <baadiyassine51@gmail.com>
2026-05-17 12:52:58 +0100

    C07 done

---
+++ Yassine/C07/ex04/ft_convert_base2.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybaadi <ybaadi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/12 12:56:36 by ybaadi            #+#    #+#             */
+/*   Updated: 2026/05/14 12:52:42 by ybaadi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int     ft_strlen(char *str)
+{
+    int     index;
+
+    index = 0;
+    while (str[index] != '\0')
+    {
+        index++;
+    }
+    return (index);
+}
+
+// BASE: must not contain the whitespaces or (-, +), nor repeated letters
+// and its size must be larger than 2.
+int     is_base_valid(char *base)
+{
+    int     i;
+    int     j;
+
+    i = 0;
+    while (base[i])
+    {
+        if (base[i] == ' ' || (base[i] >= '\t' && base[i] <= '\r'))
+            return (0);
+        if (base[i] == '-' || base[i] == '+' )
+            return (0);
+
+        j = i + 1;
+        while (base[j])
+        {
+            if (base[i] == base[j])
+            {
+                return (0);
+            }
+            j++;
+        }
+        i++;
+    }
+    return (i);
+}
+
+int     check_and_getidxbase(char c, char *base)
+{
+    int     i;
+
+    i = 0;
+    while (base[i])
+    {
+        if (c == base[i])
+        {
+            return (i);
+        }
+        i++;
+    }
+    // Harf makaynch f base (talbin f subject, base maykonoch - or +)
+    return (-1);
+}
+
+// convert initial portion of the str from specific base to int
+// expected the rule of ft_atoi function
+int     ft_atoi_base(char *str, char *base)
+{
+    /*
+        unsigned hna bach n9dr nkhzan fih 2147483648 li makaynach f int,
+        but ila drbto f -1 flakhar ghaywli valide f int (-2147483648)
+        w hasab subject maghadich y3tiwna number kharj range dyal int
+    */
+    unsigned int    result;
+    int             base_nbr;
+    int             idx_base;
+    int             sign;
+    int             i;
+
+    i = 0;
+    while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+        i++;
+
+    sign = 1;
+    while (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i++] == '-')
+            sign *= -1;
+    }
+
+    base_nbr = ft_strlen(base);
+    result = 0;
+    while (str[i])
+    {
+        idx_base = check_and_getidxbase(str[i], base);
+        if (idx_base == -1)
+            break;
+        result = (result * base_nbr) + idx_base;
+        i++;
+    }
+    return ((int)(result * sign));
+}
